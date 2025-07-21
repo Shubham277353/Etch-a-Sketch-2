b6240b8d0e234dfbe141c2c26fc92ebfdf1cd9e7
Shubham277353 <golubhandari405@gmail.com>
2025-07-21 20:53:06 +0530

    create 16 x 16 = 256 divs

---
+++ Script.js
@@ -0,0 +1,9 @@
+const container = document.querySelector(".container");
+for (let i = 0; i < 16; i++) {
+  for (let i = 0; i < 16; i++) {
+    const divs = document.createElement("div");
+    divs.classList = "squareDivs";
+  }
+}
+
+
