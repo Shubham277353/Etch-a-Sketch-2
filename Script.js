057dbf938663f1ef1cad46286686025d2d22a606
Shubham277353 <golubhandari405@gmail.com>
2025-07-21 23:04:48 +0530

    add drawing feature
    you can draw or sketch now with a create new button
    which can change the grid size

--- Script.js
+++ Script.js
@@ -1,9 +1,30 @@
 const container = document.querySelector(".container");
-for (let i = 0; i < 16; i++) {
-  for (let i = 0; i < 16; i++) {
+const newGrid = document.querySelector("#btn");
+const resetBtn = document.querySelector(".reset");
+
+function createNewGrid(size) {
+  container.innerHTML = "";
+  const totalSquare = size * size;
+  const squareSize = 960 / size;
+
+  for (let j = 1; j <= totalSquare; j++) {
     const divs = document.createElement("div");
-    divs.classList = "squareDivs";
+    divs.classList.add("squareDivs");
+
+    divs.style.height = `${squareSize}`;
+    divs.style.width = `${squareSize}`;
+
+    divs.addEventListener("mouseenter", () => {
+      div.style.backgroundColor = "black";
+    });
+
+    container.appendChild(divs);
   }
 }
 
+createNewGrid(16);
 
+newGrid.addEventListener("click", () => {
+  const size = prompt("Enter grid size (1-100):");
+  createNewGrid(size);
+});
